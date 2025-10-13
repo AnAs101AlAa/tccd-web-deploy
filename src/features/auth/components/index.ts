1aefffecb52507d17c672b12ea2b11c3a271c8bb
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-10-13 20:40:25 +0300

    feat: add SubmitButton component for auth forms with loading state

---
+++ src/features/auth/components/index.ts
@@ -0,0 +1,5 @@
+export { FormInput } from "./FormInput";
+export type { FormInputProps } from "./FormInput";
+
+export { SubmitButton } from "./SubmitButton";
+export type { SubmitButtonProps } from "./SubmitButton";
