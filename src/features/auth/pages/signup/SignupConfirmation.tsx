851a6cf1487e3a81b33f1bacf264ad0def68a2e5
Darkosylver <darkosylver27@gmail.com>
2025-10-13 23:36:00 +0300

    Sign Up confirmation page conditionally rendered

---
+++ src/features/auth/pages/signup/SignupConfirmation.tsx
@@ -0,0 +1,82 @@
+import {
+  selectUserFullName,
+  selectUserStatus,
+  useAppSelector,
+} from "@/shared/store";
+import type { UserStatus } from "@/shared/types";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ErrorScreen, InfoScreen, SuccessScreen } from "tccd-ui";
+
+export default function SingupConfirmation() {
+  const navigate = useNavigate();
+  const userStatus: UserStatus = useAppSelector(selectUserStatus);
+  const userFullName: string = useAppSelector(selectUserFullName);
+  const userFirstName: string = userFullName.substring(
+    0,
+    userFullName.indexOf(" ")
+  );
+
+  useEffect(() => {
+    if (userStatus === "Approved") {
+      const timer = setTimeout(() => {
+        navigate("/"); //speculatory home page sublink
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [userStatus, navigate]);
+
+  if (!userStatus) {
+    return (
+      <ErrorScreen
+        title="Error Logging In"
+        message="There was an error loggin you in, please try again later or contact support"
+      />
+    );
+  }
+
+  switch (userStatus) {
+    case "Pending":
+      return (
+        <InfoScreen
+          title="Account Under Review"
+          message="Your account is under review for the time being. Please check back later or contact support."
+        />
+      );
+
+    case "Rejected":
+      return (
+        <ErrorScreen
+          title="Account Registration Rejected"
+          message="Your account registration has been rejected. If you wish to appeal this decision, please contact support."
+          showAdditionalInfo={false}
+        />
+      );
+
+    case "Banned":
+      return (
+        <ErrorScreen
+          title="Account Banned"
+          message="Your account has been banned due to breaking Community Guidelines. If you wish to appeal this decision, please contact support."
+          showAdditionalInfo={false}
+        />
+      );
+
+    case "Approved":
+      return (
+        <SuccessScreen
+          title={`Welcome, ${userFirstName}!`}
+          message="Your account has been successfully created. You will be redirected to the home page shortly."
+        />
+      );
+
+    default:
+      return (
+        <ErrorScreen
+          title="Unexpected Error"
+          message="An unexpected error occurred. Please contact support."
+        />
+      );
+  }
+}
