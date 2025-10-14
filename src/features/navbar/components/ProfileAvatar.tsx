63c2bb33cf6b766b49d84cf297694a5879966bb6
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2025-10-14 21:15:55 +0300

    finals

--- src/features/navbar/components/ProfileAvatar.tsx
+++ src/features/navbar/components/ProfileAvatar.tsx
@@ -14,7 +14,7 @@ const ProfileAvatar = ({ isAuthenticated, onClick }: ProfileAvatarProps) => {
           aria-hidden
           className="absolute -bottom-1 left-1/2 -translate-x-1/2 h-2.5 w-10 md:w-14 rounded-full bg-black/10 blur-md"
         />
-        <div className="rounded-full w-[44px] sm:w-[50px] h-[44px] sm:h-[50px] md:w-[43px] md:h-[43px] xl:w-[48px] xl:h-[48px] bg-white border border-[#2A5F80] overflow-hidden flex items-center justify-center">
+        <div className="rounded-full w-[46px] sm:w-[53px] h-[46px] sm:h-[53px] md:w-[42px] md:h-[42px] xl:w-[44px] xl:h-[44px] bg-white border border-[#2A5F80] overflow-hidden flex items-center justify-center">
           {!isAuthenticated ? (
             <div className="flex items-center justify-center w-full h-full">
               <FiLogIn className="text-[#2A5F80] text-[20px] md:text-[28px]" />
