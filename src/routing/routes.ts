c4053c265e12a2caa3f939919485cff7f5314dfd
hazemyasser123 <hazemyasser6@gmail.com>
2025-10-17 11:51:31 +0300

    Add About Us feature with board and founders info

--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -1,7 +1,8 @@
-import { LoginPage, SignupPage, SignupConfirmation } from "@/features";
+import { LoginPage, SignupPage, SignupConfirmation, AboutUsPage } from "@/features";
 
 export const routes = [
   { path: "/login", Component: LoginPage },
   { path: "/sign-up", Component: SignupPage },
   { path: "/sign-up/confirmation", Component: SignupConfirmation },
+  { path: "/about-us", Component: AboutUsPage },
 ];
