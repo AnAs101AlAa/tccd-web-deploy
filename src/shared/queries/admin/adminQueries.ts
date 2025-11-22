f3c7529390bdb65700077d6005aa511befb1dec6
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-11-22 16:26:35 +0200

    refactor: comment out unused locationApi import in adminQueries

--- src/shared/queries/admin/adminQueries.ts
+++ src/shared/queries/admin/adminQueries.ts
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { locationApi } from "./adminApi";
+// import { locationApi } from "./adminApi";
 import { getDataBlob } from "@/shared/utils";
 import type {
   Location,
