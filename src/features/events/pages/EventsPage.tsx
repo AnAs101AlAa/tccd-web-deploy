54334e296ab742080a1009ca0bf028a6122ed968
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2026-01-25 21:30:46 +0200

    style: Add rounded corners to Upcoming and Past Events sections for improved UI

--- src/features/events/pages/EventsPage.tsx
+++ src/features/events/pages/EventsPage.tsx
@@ -105,7 +105,7 @@ const EventsPage = () => {
 
         <main className="w-full xl:w-[80%] xl:mx-auto px-0 md:px-6 py-5">
           {/* Upcoming Events Section */}
-          <section className="mb-6 md:mb-10 shadow-lg p-2 md:p-3 relative pb-5 md:pb-7 bg-background">
+          <section className="mb-6 md:mb-10 shadow-lg p-2 md:p-3 relative pb-5 md:pb-7 bg-background rounded-t-2xl">
             <div className="flex flex-row items-center justify-between gap-3 sm:gap-0 mb-2">
               <h2 className="text-2xl sm:text-3xl font-bold text-secondary">
                 Upcoming Events
@@ -141,7 +141,7 @@ const EventsPage = () => {
           </section>
 
           {/* Past Events Section */}
-          <section className="shadow-lg p-2 md:p-3 relative pb-3 md:pb-4 bg-background">
+          <section className="shadow-lg p-2 md:p-3 relative pb-3 md:pb-4 bg-background rounded-t-2xl">
             <div className="flex flex-row items-center justify-between gap-3 sm:gap-0 mb-3 md:mb-6">
               <h2 className="text-2xl sm:text-3xl font-bold text-secondary">
                 Past Events
@@ -159,7 +159,7 @@ const EventsPage = () => {
             <ViewAllButton
               onClick={() => toggleViewAllPast({ route: "/past-events" })}
             />
-            <div className="h-1 bg-gradient-to-r from-secondary via-primary to-secondary rounded-full absolute left-0 right-0 w-full bottom-0"></div> 
+            <div className="h-1 bg-gradient-to-r from-secondary via-primary to-secondary rounded-full absolute left-0 right-0 w-full bottom-0"></div>
           </section>
         </main>
       </div>
