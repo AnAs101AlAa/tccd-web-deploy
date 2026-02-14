a41174d1a00c867cdf4a9900d92740d677bd81d4
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2026-02-14 19:58:28 +0200

    refactor: Remove unused server proxy configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },  
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://tccd-web-backend.runasp.net/", // replace with your backend URL
-        changeOrigin: true,
-        secure: true, // because target is https
-        // IMPORTANT: do NOT rewrite /api -> "" because your backend expects /api/...
-        // rewrite: (p) => p.replace(/^\/api/, ""), // <-- leave this OUT
-      },
     },
   },
 });
