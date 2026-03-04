17f5b707e6d14d4b09bab26515e7008d4b23176d
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2026-03-04 02:53:03 +0200

    Enhance RegistrationConfirmationModal with confirmation input; update modal title and message for clarity

--- src/features/events/components/RegistrationConfirmationModal.tsx
+++ src/features/events/components/RegistrationConfirmationModal.tsx
@@ -1,4 +1,5 @@
-import { Modal, Button } from "tccd-ui";
+import { Modal, Button, InputField } from "tccd-ui";
+import { useState } from "react";
 
 export default function RegistrationConfirmationModal({
   isOpen,
@@ -11,12 +12,23 @@ export default function RegistrationConfirmationModal({
   onConfirm: () => void;
   isSubmitting: boolean;
 }) {
+    const [confirmationText, setConfirmationText] = useState("");
+
     return (
-        <Modal isOpen={isOpen} onClose={onClose} title="Confirm Registration">
-            <p className="text-contrast mb-4 text-center">Are you sure you want to register for this time slot? you will <strong>NOT be permitted</strong> entry on any different slots during the event.</p>
-            <div className="flex justify-center space-x-2">
+        <Modal isOpen={isOpen} onClose={onClose} title="Important Notice">
+            <p className="text-contrast mb-4 mt-2 text-center">Are you sure you want to register for this time slot? you will <strong>NOT be permitted</strong> entry on any different slots during the event.</p>
+            <InputField
+                labelClassName="text-[13px] md:text-[14px] lg:text-[15px] text-gray-600 mb-1"
+                id="confirm-registration"
+                label="Please type 'CONFIRM' to proceed"
+                placeholder="CONFIRM"
+                onChange={(e) => setConfirmationText(e.target.value)}
+                value={confirmationText}
+            />
+
+            <div className="flex justify-center space-x-2 mt-5">
                 <Button type="basic" onClick={onClose} buttonText="Cancel" disabled={isSubmitting} />
-                <Button type="primary" onClick={() => onConfirm()} buttonText="Confirm" loading={isSubmitting} disabled={isSubmitting} />
+                <Button type="primary" onClick={() => onConfirm()} buttonText="Confirm" loading={isSubmitting} disabled={isSubmitting || confirmationText !== "CONFIRM"} />
             </div>
         </Modal>
     );
